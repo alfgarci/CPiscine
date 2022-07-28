d2889444108ab1e69140be2c848388b4329aaf58
Alfonso Garcia <alfgarci@student.42madrid.com>
2022-07-28 17:37:54 +0200

    C06 Succeeded with 100%

--- C06/ex00/ft_print_program_name.c
+++ C06/ex00/ft_print_program_name.c
@@ -1,16 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ten_queens_puzzle.c                                :+:      :+:    :+:   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alfgarci <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/20 10:53:31 by alfgarci          #+#    #+#             */
-/*   Updated: 2022/07/20 20:53:55 by alfgarci         ###   ########.fr       */
+/*   Created: 2022/07/19 06:53:11 by alfgarci          #+#    #+#             */
+/*   Updated: 2022/07/19 07:18:13 by alfgarci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_ten_queen_puzzle(void)
+#include <unistd.h>
+
+void	ft_putstr(char *str)
 {
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	if (ac > 0)
+		ft_putstr(av[0]);
+	write(1, "\n", 1);
 	return (0);
 }
---
+++ C06/ex03/ft_sort_params.c
@@ -0,0 +1,62 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alfgarci <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/19 07:40:54 by alfgarci          #+#    #+#             */
+/*   Updated: 2022/07/19 10:58:02 by alfgarci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while ((s1[i] == s2[i]) && (s1[i] != '\0' || s2[i] != '\0'))
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+int	main(int ac, char **av)
+{
+	int		i;
+	char	*tmp;
+
+	i = 1;
+	while (i < ac - 1)
+	{
+		if (ft_strcmp(av[i], av[i + 1]) > 0)
+		{
+			tmp = av[i];
+			av[i] = av[i + 1];
+			av[i + 1] = tmp;
+			i = 1;
+		}
+		else
+			i++;
+	}
+	i = 0;
+	while (++i < ac)
+	{
+		ft_putstr(av[i]);
+		write(1, "\n", 1);
+	}
+	return (0);
+}
